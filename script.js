f624193a54bd02172ed36a8d532a1809eb63d649
mrbhi <taofeeqworld@hotmail.com>
2022-10-17 09:57:39 -0700

    Added the mobile menu JavaScript toggle function

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-  const openBtn = document.getElementById('openBtn');
-  const closeBtn = document.getElementById('closeBtn');
-  const overlay = document.getElementById('overlay');
-  openBtn.onclick = () => {
-  overlay.style.display = 'block';
-  document.body.style.position = 'fixed';
-};
-  closeBtn.onclick = () => {
-  overlay.style.display = 'none';
-  document.body.style.position = 'relative';
-};
-function close() {
-  overlay.style.display = 'none';
-  document.body.style.position = 'relative';
-}
-document.querySelectorAll('.overlay_link').forEach((btn) => {
-  btn.onclick = () => { close(); };
+const humburger = document.querySelector('.humburger');
+const navMenu = document.querySelector('#overlay');
+const closeImage = document.querySelector('.closeImg');
+
+humburger.addEventListener('click', () => {
+humburger.classList.toggle('visible');
+navMenu.classList.toggle('visible');
 });
 
-openBtn.onclick = () => {
-  overlay.style.display = 'block';
-  document.body.style.position = 'fixed';
-};
+closeImage.addEventListener('click', () => {
+navMenu.classList.remove('visible');
+});
 
-closeBtn.onclick = () => {
-  overlay.style.display = 'none';
-  document.body.style.position = 'relative';
-};
-  function close () {
-    overlay.style.display = 'none';
-    document.body.style.position = 'relative';
-  }
-  document.querySelectorAll('.overlay_link').forEach((btn) => {
-    btn.onclick = () => { close(); };
-  });
+document.querySelectorAll('.overlay_link').forEach((l) => l.addEventListener('click', () => {
+  navMenu.classList.remove('visible');
+  }));
