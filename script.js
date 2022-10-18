0914992a9ea7c749ad2f6c9ac1d149c3bdf4284f
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-10-17 20:27:52 -0700

    corrected eslint errors

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const navMenu = document.querySelector('#overlay');
 const closeImage = document.querySelector('.closeImg');
 
 humburger.addEventListener('click', () => {
-humburger.classList.toggle('visible');
-navMenu.classList.toggle('visible');
+  humburger.classList.toggle('visible');
+  navMenu.classList.toggle('visible');
 });
 
 closeImage.addEventListener('click', () => {
-navMenu.classList.remove('visible');
+  navMenu.classList.remove('visible');
 });
 
 document.querySelectorAll('.overlay_link').forEach((l) => l.addEventListener('click', () => {
   navMenu.classList.remove('visible');
-  }));
+}));
