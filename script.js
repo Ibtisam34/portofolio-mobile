2ceec61292c349dbb6b528a2cabd484e6e905862
mrbhi <taofeeqworld@hotmail.com>
2022-10-20 07:30:21 -0700

    Removed the Eslint Error

--- script.js
+++ script.js
@@ -175,33 +175,33 @@ form.addEventListener('submit', (event) => {
   }
 });
 
-let formData = {
+const formData = {
   name: '',
   email: '',
-  message: ''
-}
+  message: '',
+};
 
-let nameInput = document.getElementById('fname');
-let emailInput = document.getElementById('email');
-let textInput = document.getElementById('textarea')
+const nameInput = document.getElementById('fname');
+const emailInput = document.getElementById('email');
+const textInput = document.getElementById('textarea');
 
 nameInput.addEventListener('input', () => {
   formData.name = nameInput.value;
-  localStorage.setItem('data', JSON.stringify(formData))
+  localStorage.setItem('data', JSON.stringify(formData));
 });
 
 emailInput.addEventListener('input', () => {
   formData.email = emailInput.value;
-  localStorage.setItem('data', JSON.stringify(formData))
+  localStorage.setItem('data', JSON.stringify(formData));
 });
 
 textInput.addEventListener('input', () => {
   formData.message = textInput.value;
-  localStorage.setItem('data', JSON.stringify(formData))
+  localStorage.setItem('data', JSON.stringify(formData));
 });
 
 if (localStorage.getItem('data')) {
-  let formObject = localStorage.getItem('data')
+  let formObject = localStorage.getItem('data');
   formObject = JSON.parse(formObject);
 
   document.getElementById('fname').value = formObject.name;
