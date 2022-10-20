0767eaa33ff6d0d4b0d4936975d7b407e2e9b63f
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-10-19 22:19:56 -0700

    modified html & css

--- script.js
+++ script.js
@@ -129,3 +129,52 @@ multiProject.addEventListener('click', () => {
   css.classList.add('show');
   recent.classList.add('active');
 });
+
+function showMessage(input, message) {
+  const msg = input.parentNode.querySelector('small');
+  const emailInput = document.getElementById('email');
+  msg.innerText = message;
+
+  // modify the class for the input
+  
+  msg.className = 'error';
+  emailInput.className = 'error-box';
+}
+// Function to show error
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+// Check if email is in lowercase
+
+function validateEmail(input, invalidMsg) {
+  const email = input.value.trim();
+  if (!(email === String(email).toLowerCase())) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.querySelector('#form-id');
+
+const EMAIL_INVALID = 'Enter email in lowercase';
+
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
+
+  // validate the form
+
+  const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
+
+  // if valid, submit the form.
+
+  if (emailValid) {
+    form.submit();
+  }
+});
+
+
+
+
